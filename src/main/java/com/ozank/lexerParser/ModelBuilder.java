17455dd4024036b6c14771c2d23940816bb18915
ozan-k <ozan.kah@gmail.com>
2023-06-22 14:42:56 +0200

    done lexer/parser prototype

--- src/main/java/com/ozank/lexerParser/ModelBuilder.java
+++ src/main/java/com/ozank/lexerParser/ModelBuilder.java
@@ -10,46 +10,72 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ModelBuilder {
-
     private final List<Reaction> reactions = new ArrayList<>();
     private final Map<String,Integer> initialState = new HashMap<>();
     private double endTime;
+    private double scalingFactor = 1;
+    private int every = 1;
+    private int interval = 0;
 
     public ModelBuilder(String file) {
+        file = file.replaceAll("//.*?(\\n|\\z)","");
         file  = file.replaceAll("\\s*","");
-        String regex = "reactions(.*)initialstate(.*)endtime(.+?);.*";
+        String regex = "reactions(.*)initialstate(.*)endtime(.+?);(.*)";
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(file);
-        if (m.find( )) {
-
+        if (m.find()) {
+            setOptionalParameters(m.group(4));
+            parseReactions(m.group(1));
+            parseInitialState(m.group(2));
+            try {
+                endTime = Double.parseDouble(m.group(3));
+            } catch (IndexOutOfBoundsException e){
+                System.out.println(e);
+            }
         }
-        parseReactions(m.group(1));
-        parseInitialState(m.group(2));
-        try {
-            endTime = Double.parseDouble(m.group(3));
-        } catch (IndexOutOfBoundsException e){
-            System.out.println(e);
+
+        for (String s : initialState.keySet()){
+        System.out.println(s + " : " + initialState.get(s));
         }
         for (Reaction r : reactions){
             System.out.println(r);
         }
-        for (String s : initialState.keySet()){
-            System.out.println(s + " : " + initialState.get(s));
-        }
-        System.out.println("");
-        System.out.println(endTime);
     }
 
+    private void setOptionalParameters(String optionalParameters){
+        String regex = ".*every(.+?);.*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m = pattern.matcher(optionalParameters);
+        if (m.find()) {
+            every = Integer.parseInt(m.group(1));
+        }
+        // -------------------------------------
+        regex = ".*scalingfactor(.+?);.*";
+        pattern = Pattern.compile(regex);
+        m = pattern.matcher(optionalParameters);
+        if (m.find()){
+            scalingFactor = Double.parseDouble(m.group(1));
+        }
+        // -------------------------------------
+        regex = ".*interval(.+?);.*";
+        pattern = Pattern.compile(regex);
+        m = pattern.matcher(optionalParameters);
+        if (m.find()){
+            interval = Integer.parseInt(m.group(1));
+        }
+    }
     private void parseReactions(String reactionsString){
         String regex = "(.*):(.*)->(.*):(.*)";
         Pattern pattern = Pattern.compile(regex);
         for (String reaction: reactionsString.split(";")){
             Matcher m = pattern.matcher(reaction);
-            if (m.find( )) {
+            if (m.find()) {
                 String id = m.group(1);
                 Map<String,Integer> left = organizeReactants(m.group(2));
+                double length_factor = (double) left.values().stream().reduce(0, Integer::sum) -1;
                 Map<String,Integer> right = organizeReactants(m.group(3));
-                double rate = Double.parseDouble(m.group(4));
+                // https://en.wikipedia.org/wiki/Rate_equation
+                double rate = Double.parseDouble(m.group(4)) / Math.pow(scalingFactor,length_factor);
                 reactions.add(new Reaction(id,rate,left,right));
             }
         }
@@ -71,11 +97,11 @@ public class ModelBuilder {
         Pattern pattern = Pattern.compile(regex);
         for (String string : initialStateString.split(";")){
             Matcher m = pattern.matcher(string);
-            if (m.find( )) {
+            if (m.find()) {
                 String molecule = m.group(1);
                 Integer count;
                 try {
-                    count = Integer.parseInt(m.group(2));
+                    count = (int) (Double.parseDouble(m.group(2)) * scalingFactor);
                 } catch (Exception e){
                     throw new IllegalArgumentException();
                 }
@@ -100,4 +126,28 @@ public class ModelBuilder {
         return endTime;
     }
 
+    public int getEvery(){
+        return every;
+    }
+    
+    public int getInterval(){
+        return interval;
+    }
+
 }
+
+
+//    let rec scale_reactions reac_list scaling_factor = match reac_list with [] -> [] |
+//        (name,left,right,rate)::tail -> let length_factor = float_of_int ((List.length left) - 1)
+//        in (name,left,right,rate /. ( scaling_factor ** length_factor ) )::(scale_reactions tail scaling_factor)
+
+
+//for (String s : initialState.keySet()){
+//        System.out.println(s + " : " + initialState.get(s));
+//        }
+//        for (Reaction r : reactions){
+//            System.out.println(r);
+//        }
+//
+//        System.out.println("");
+//        System.out.println(endTime);
